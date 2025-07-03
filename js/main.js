62a0776cc3f089e782f19e96e0e94ede6cf5dac1
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-07-03 16:57:06 -0400

    wk06 and wk03/interactions

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // which menu to keep open
   const url = window.location.href;
-  let menuWeek = 6;
+  let menuWeek = 7;
   const weekPos = url.indexOf("week");
   const dataPos = url.indexOf("datasets");
   const hashNums = window.location.hash.match(/\d+/g);
